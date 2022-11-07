f4c0d99261539c23ab7cd47643f8235a76df9931
andrewmiladd <andrewmilad11@gmail.com>
2022-11-07 20:47:26 +0200

    Gathered From The User The inputs

--- src/Components/Forms.tsx
+++ src/Components/Forms.tsx
@@ -1,26 +1,43 @@
-import React  from "react";
+import React, { useState } from "react";
 import "./Forms.css";
 
 function Form() {
+  const [email, setEmail] = useState("");
+  const [message, setMessage] = useState("");
+
+  let emailInputHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setEmail(e.target.value);
+  };
+  let messageInputhandler = (e: any) => {
+    setMessage(e.target.value);
+  };
+
   return (
     <div>
-      <section></section>
       <div className="container">
         <form>
-          <h1>Send Your Message</h1>
-
+          <h1>Your Mail Sender</h1>
           <div className="item">
             <label style={{ fontSize: "30px" }}> To </label>
-            <input type="email" placeholder="Enter a valid email" />
+            <input
+              type="email"
+              placeholder="Enter a valid email"
+              required
+              value={email}
+              onChange={emailInputHandler}
+            />
           </div>
           <label style={{ fontSize: "30px" }}>Your Message</label>
           <textarea
             cols={35}
             rows={10}
             placeholder="Enter Your Message"
+            required
+            value={message}
+            onChange={messageInputhandler}
           ></textarea>
 
-          <button>Send Your Message</button>
+          <button type="submit">Send Your Message</button>
         </form>
       </div>
     </div>
