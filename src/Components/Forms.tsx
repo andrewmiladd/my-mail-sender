3a83e70cbc801a704569baaa1a68a12743f9f7d9
andrewmiladd <andrewmilad11@gmail.com>
2022-11-07 16:49:13 +0200

    Deleted Unecessary Files

--- src/Components/Forms.tsx
+++ src/Components/Forms.tsx
@@ -1,5 +1,19 @@
 import React from "react";
-function Form(){
-
+function Form() {
+  return (
+    <div>
+      <form>
+        <label htmlFor="email">To</label>
+        <br />
+        <input type="email" id="email" />
+        <br />
+        <label htmlFor="message">Message</label>
+        <br />
+        <textarea id="message"></textarea>
+        <br />
+        <button type="submit">Send!</button>
+      </form>
+    </div>
+  );
 }
 export default Form;
