05550787d231eb9e74112243607e24ef1366a6f4
Bichoy Messiha <bichoy.messiha@indigen.com>
2022-11-12 15:11:21 +0200

    chore: install prettier

--- src/Components/Forms.tsx
+++ src/Components/Forms.tsx
@@ -2,48 +2,48 @@ import React, { useState } from "react";
 import "./Forms.css";
 
 function Form() {
-  type stringState = [string, React.Dispatch<React.SetStateAction<string>>];
+    type stringState = [string, React.Dispatch<React.SetStateAction<string>>];
 
-  const [email, setEmail]: stringState = useState("");
-  const [message, setMessage]: stringState = useState("");
+    const [email, setEmail]: stringState = useState("");
+    const [message, setMessage]: stringState = useState("");
 
-  let emailInputHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setEmail(e.target.value);
-  };
-  let messageInputhandler = (e: any) => {
-    setMessage(e.target.value);
-  };
+    let emailInputHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setEmail(e.target.value);
+    };
+    let messageInputhandler = (e: any) => {
+        setMessage(e.target.value);
+    };
 
-  return (
-    <div>
-      <div className="container">
-        <form>
-          <h1>Your Mail Sender</h1>
-          <div className="item">
-            <label> To </label>
-            <input
-              type="email"
-              multiple
-              placeholder="Enter a valid email"
-              required
-              value={email}
-              onChange={emailInputHandler}
-            />
-          </div>
-          <label>Your Message</label>
-          <textarea
-            cols={35}
-            rows={10}
-            placeholder="Enter Your Message"
-            required
-            value={message}
-            onChange={messageInputhandler}
-          ></textarea>
+    return (
+        <div>
+            <div className="container">
+                <form>
+                    <h1>Your Mail Sender</h1>
+                    <div className="item">
+                        <label> To </label>
+                        <input
+                            type="email"
+                            multiple
+                            placeholder="Enter a valid email"
+                            required
+                            value={email}
+                            onChange={emailInputHandler}
+                        />
+                    </div>
+                    <label>Your Message</label>
+                    <textarea
+                        cols={35}
+                        rows={10}
+                        placeholder="Enter Your Message"
+                        required
+                        value={message}
+                        onChange={messageInputhandler}
+                    ></textarea>
 
-          <button type="submit">Send Your Message</button>
-        </form>
-      </div>
-    </div>
-  );
+                    <button type="submit">Send Your Message</button>
+                </form>
+            </div>
+        </div>
+    );
 }
 export default Form;
