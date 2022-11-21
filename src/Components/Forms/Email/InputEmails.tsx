065e204220fc2b134b435c6f494b6869337606ea
andrewmiladd <andrewmilad11@gmail.com>
2022-11-21 17:25:09 +0200

    Started Validation with formik

--- src/Components/Forms/Email/InputEmails.tsx
+++ src/Components/Forms/Email/InputEmails.tsx
@@ -1,43 +1,27 @@
-import React, { useState, ChangeEvent, KeyboardEvent } from "react";
+import React, { useState, KeyboardEvent } from "react";
 import { EmailsList } from "./EmailsList";
-import { ErrorMessages } from "./ErrorMessages";
 
 //Adding the data to FormSendMail Component
 interface Props {
     loggerDataOnSuccess: React.Dispatch<React.SetStateAction<string[]>>;
     classNameFromParent: string;
+    emailValue: string;
+    handleChange: any;
 }
-export const InputEmails = ({ loggerDataOnSuccess, classNameFromParent }: Props) => {
-    const [newEnteredEmail, setNewEnteredEmail] = useState("");
+
+export const InputEmails = ({
+    loggerDataOnSuccess,
+    classNameFromParent,
+    emailValue,
+    handleChange,
+}: Props) => {
     const [allEmails, setAllEmails] = useState([] as string[]);
-    const [isEmailState, setIsEmailState] = useState(true);
-    const [notRepeatedState, setNotRepeatedState] = useState(true);
-
-    let onNewEmailChangeHandler = (e: ChangeEvent<HTMLInputElement>) => {
-        setNewEnteredEmail(e.target.value);
-        setIsEmailState(true);
-        setNotRepeatedState(true);
-    };
-    let isEmail = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(newEnteredEmail); //Email format validation
-    let notRepeated = !allEmails.includes(newEnteredEmail); // To make sure there is not any repeated emails
-
-    let formValidation = () => {
-        if (isEmail && notRepeated) {
-            setAllEmails([...allEmails, newEnteredEmail.toLocaleLowerCase()]);
-            setNewEnteredEmail("");
-            setIsEmailState(true);
-            setNotRepeatedState(true);
-            loggerDataOnSuccess([...allEmails, newEnteredEmail.toLocaleLowerCase()]);
-        } else if (!notRepeated) {
-            setNotRepeatedState(false);
-        } else {
-            setIsEmailState(false);
-        }
-    };
+    
     let multipleMailsEventHandler = (e: KeyboardEvent<HTMLInputElement>) => {
         if (["Enter", ",", "Tab"].includes(e.key)) {
             e.preventDefault();
-            formValidation();
+            setAllEmails([...allEmails, emailValue]);
+            loggerDataOnSuccess([...allEmails, emailValue]);
         }
     };
     return (
@@ -49,12 +33,12 @@ export const InputEmails = ({ loggerDataOnSuccess, classNameFromParent }: Props)
             />
             <input
                 placeholder="Enter a valid email"
-                value={newEnteredEmail.toLocaleLowerCase().trim()}
-                onChange={onNewEmailChangeHandler}
+                value={emailValue}
+                onChange={handleChange}
+                id="email"
                 onKeyDown={multipleMailsEventHandler}
                 className={`${classNameFromParent}`}
             />
-            <ErrorMessages isEmail={isEmailState} notRepeated={notRepeatedState} />
         </>
     );
 };
--- src/Components/Forms/FormSendMail.tsx
+++ src/Components/Forms/FormSendMail.tsx
@@ -1,37 +1,54 @@
-import React, { FormEvent, useState } from "react";
+import React, { useState } from "react";
 import styles from "./FormSendMail.module.css";
 import { InputEmails } from "./Email/InputEmails";
 import { InputMessage } from "./Message/InputMessage";
+import { useFormik, FormikProps } from "formik";
+import { formFormat } from "./Validation/Validation";
 
 export const FormSendMail = () => {
     //Adding the States to receive data from children components
     const [allEmails, setAllEmails] = useState([] as string[]);
     const [newMessage, setNewMessage] = useState("");
 
-    let formSubmitHandler = (e: FormEvent<HTMLFormElement>) => {
-        e.preventDefault();
-        if (allEmails.length > 0 && newMessage.trim().length > 0) {
-            console.log([...allEmails, { message: newMessage }]);
-        } else {
-            console.log("Error, Form Refused!");
-            return false;
-        }
+    interface myValues {
+        email: string;
+    }
+
+    let newEmail: myValues = { email: "" };
+    let onSubmit = (values: any, actions: any) => {
+        console.log(allEmails, newMessage);
+        console.log(values);
+        actions.resetForm();
     };
+
+    const { values, errors, handleChange, handleSubmit, touched }: FormikProps<myValues> =
+        useFormik({
+            initialValues: newEmail,
+            onSubmit: onSubmit,
+            validationSchema: formFormat,
+        });
+    console.log(errors);
+
     return (
-        <form onSubmit={formSubmitHandler}>
+        <form onSubmit={handleSubmit}>
             <h1>Your Mail Sender</h1>
-            <div className={styles.items}>
-                <label className={styles.myLabel}> To </label>
-                <InputEmails
-                    loggerDataOnSuccess={setAllEmails}
-                    classNameFromParent={styles.myInput}
-                />
-                <label className={styles.myLabel}>Your Message</label>
-                <InputMessage
-                    className={styles.inputMessage}
-                    setNewMessageFromParent={setNewMessage}
-                />
-            </div>
+            <label className={styles.myLabel}> To </label>
+            <InputEmails
+                loggerDataOnSuccess={setAllEmails}
+                classNameFromParent={`${styles.myInput} ${
+                    errors.email && touched.email ? styles.inputError : ""
+                }`}
+                emailValue={values.email}
+                handleChange={handleChange}
+            />
+            {errors.email && touched.email && (
+                <p style={{ color: "red", margin: 0 }}>*{errors.email}</p>
+            )}
+            <label className={styles.myLabel}>Your Message</label>
+            <InputMessage
+                className={`${styles.inputMessage}`}
+                setNewMessageFromParent={setNewMessage}
+            />
             <button type="submit" className={styles.myButton}>
                 Send Your Message
             </button>
