7abbcd61aefead3904b9b886f3726227586d278d
andrewmiladd <andrewmilad11@gmail.com>
2023-01-04 15:50:02 +0200

    feat: Added Snackbar error message

--- src/Components/Forms/Login/LoginForm.tsx
+++ src/Components/Forms/Login/LoginForm.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { Button } from "@mui/material";
 import styles from "./LoginForm.module.css";
 import { useFormik } from "formik";
@@ -7,6 +7,9 @@ import { EmailInput } from "../Shared Components/EmailInput";
 import { PasswordInput } from "../Shared Components/PasswordInput";
 import axios from "axios";
 import { Link } from "react-router-dom";
+import Snackbar from "@mui/material/Snackbar";
+import Stack from "@mui/material/Stack";
+import MuiAlert, { AlertProps } from "@mui/material/Alert";
 
 interface LogInFields {
     email: string;
@@ -14,6 +17,7 @@ interface LogInFields {
 }
 let myInitialValues: LogInFields = { email: "", password: "" };
 export const LogInForm = () => {
+    const [isRightCredentials, setIsRightCredentials] = useState(true);
     const { values, errors, handleChange, handleSubmit, touched } = useFormik({
         initialValues: myInitialValues,
         onSubmit: values => {
@@ -28,38 +32,74 @@ export const LogInForm = () => {
                         window.location.href = "/";
                     }
                 })
-                .catch(error => console.log(error));
+                .catch(error => {
+                    if ((error.message = "Request failed with status code 401")) {
+                        setIsRightCredentials(false);
+                    }
+                });
         },
         validationSchema: loginFormat,
     });
+
+    const Alert = React.forwardRef<HTMLDivElement, AlertProps>(function Alert(props, ref) {
+        return <MuiAlert elevation={6} ref={ref} variant="filled" {...props} />;
+    });
+    const [open, setOpen] = useState(false);
+    const handleClick = () => {
+        setOpen(true);
+    };
+
+    const handleClose = (event?: React.SyntheticEvent | Event, reason?: string) => {
+        if (reason === "clickaway") {
+            return;
+        }
+
+        setOpen(false);
+    };
     return (
-        <form className={styles.myLogInForm} onSubmit={handleSubmit}>
-            <h2 className={styles.myHeader}>Log in</h2>
-            <EmailInput
-                emailValue={values.email}
-                handleChange={handleChange}
-                emailError={errors.email}
-                isTouched={touched.email}
-                styles={`${styles.myErrorMessages}`}
-            />
+        <>
+            {!isRightCredentials && (
+                <Stack spacing={4} sx={{ width: "100%" }}>
+                    <Snackbar open={open} onClose={handleClose}>
+                        <Alert onClose={handleClose} severity="error" sx={{ width: "100%" }}>
+                            Wrong credentials
+                        </Alert>
+                    </Snackbar>
+                </Stack>
+            )}
+            <form className={styles.myLogInForm} onSubmit={handleSubmit}>
+                <h2 className={styles.myHeader}>Log in</h2>
+                <EmailInput
+                    emailValue={values.email}
+                    handleChange={handleChange}
+                    emailError={errors.email}
+                    isTouched={touched.email}
+                    styles={`${styles.myErrorMessages}`}
+                />
 
-            <PasswordInput
-                passwordValue={values.password}
-                handleChange={handleChange}
-                passwordError={errors.password}
-                isTouched={touched.password}
-                styles={`${styles.myErrorMessages}`}
-            />
+                <PasswordInput
+                    passwordValue={values.password}
+                    handleChange={handleChange}
+                    passwordError={errors.password}
+                    isTouched={touched.password}
+                    styles={`${styles.myErrorMessages}`}
+                />
 
-            <Button type="submit" variant="contained" id={styles.my__button}>
-                Login
-            </Button>
-            <p className={styles.myMessage}>
-                Don't have an account?
-                <Link to="/signUp" className={styles.myMessage} style={{ marginLeft: "7px" }}>
-                    Sign up
-                </Link>
-            </p>
-        </form>
+                <Button
+                    type="submit"
+                    variant="contained"
+                    id={styles.my__button}
+                    onClick={handleClick}
+                >
+                    Login
+                </Button>
+                <p className={styles.myMessage}>
+                    Don't have an account?
+                    <Link to="/signUp" className={styles.myMessage} style={{ marginLeft: "7px" }}>
+                        Sign up
+                    </Link>
+                </p>
+            </form>
+        </>
     );
 };
